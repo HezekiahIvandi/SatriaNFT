b2a63f3fde7edbe42f9e0c45712d59efe1315918
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-07-07 21:00:37 +0700

    Menambahkan onscroll animation

--- public/js/home.js
+++ public/js/home.js
@@ -7,3 +7,25 @@ content.forEach((item) => {
   duplicatedItem.setAttribute("aria-hidden", true);
   cardsCon.appendChild(duplicatedItem);
 });
+
+//onscroll animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+//targetkan element langkah button
+const animatedElements = document.querySelectorAll(".langkah-button");
+animatedElements.forEach((element) => {
+  observer.observe(element);
+});
+//targetkan element wallet button
+const animatedElements2 = document.querySelectorAll(".wallet-button");
+animatedElements2.forEach((element) => {
+  observer.observe(element);
+});
