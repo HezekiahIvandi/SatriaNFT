618116fd33f6ccb31ce64af076fefabd2f9aba15
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-07-18 12:53:21 +0700

    pprogress login

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { cardsInfo } = require("../card-info");
 const { JelajahiNFTs, ExtraJelajahiNFTs } = require("../Jelajahi-NFTs");
+const { kreatorInfo } = require("../kreator-info");
 router.get("/", (req, res) => {
   res.render("home.ejs", {
     title: "Home",
@@ -9,6 +10,13 @@ router.get("/", (req, res) => {
     cardsInfo: cardsInfo,
     JelajahiNFTs: JelajahiNFTs,
     ExtraJelajahiNFTs: ExtraJelajahiNFTs,
+    kreatorInfo: kreatorInfo,
+  });
+});
+router.get("/login", (req, res) => {
+  res.render("login.ejs", {
+    title: "Login",
+    layout: "mainLayout.ejs",
   });
 });
 
